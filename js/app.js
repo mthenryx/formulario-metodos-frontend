4d3a48180f39dc45ac8861d19ee5496d923ca36c
mthenryx <09matheushenry@gmail.com>
2026-05-28 10:56:03 -0300

    Subindo tudo o que eu fiz em casa, falta apenas a parte do PUT

--- js/app.js
+++ js/app.js
@@ -2,14 +2,32 @@
 
 import { getContatos, getContato, postContato, putContato, deleteContato } from "./contatos.js"
 
-function estruturarJson() {
+function validarCampos(nome, telefone, email, endereco, cidade) {
+    let result = { "status": false, "message": "" }
 
+    if (nome.value == "") {
+        result.message = "Nome não foi preenchido"
+    } else if (telefone.value == "") {
+        result.message = "Telefone não foi preenchido"
+    } else if (email.value == "") {
+        result.message = "E-mail não foi preenchido"
+    } else if (endereco.value == "") {
+        result.message = "Endereco não foi preenchido"
+    } else if (cidade.value == "") {
+        result.message = "Cidade não foi preenchido"
+    } else {
+        result.status = true
+    }
+
+    return result
 }
 
 async function criarCards() {
     const contatos = await getContatos()
 
     const cards = document.getElementById("cards")
+
+    cards.innerHTML = ""
 
     for (let contato of contatos) {
 
@@ -30,11 +48,11 @@ async function criarCards() {
 
         const foto = document.createElement("img")
         foto.className = "foto"
- 
-        if(contato.foto == ""){
+
+        if (contato.foto == "") {
             foto.src = "./img/usuario-sem-icone.png"
             foto.alt = "Usuário sem foto"
-        }else{
+        } else {
             foto.src = `${contato.foto}`
             foto.alt = "Foto do usuário"
         }
@@ -62,19 +80,88 @@ async function criarCards() {
         buttonDelete.textContent = "X"
 
         botoes.append(buttonUpdate, buttonDelete)
-        dados.append(id, nome, celular, foto, email, endereco, cidade, botoes)
+        dados.append(id, nome, celular, email, endereco, cidade, botoes)
         card.append(foto, dados)
         cards.append(card)
     }
-}   
+}
 
-async function deletarEAtualizar(id){
+async function deletarEAtualizar(id) {
     await deleteContato(id)
+    await criarCards()
 }
 
-function atualizarDados(contato) {
+async function atualizarDados(contato) {
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    })
 
+    const nome = document.getElementById("nomeInput")
+    nome.value = contato.nome
+    const telefone = document.getElementById("numeroInput")
+    telefone.value = contato.celular
+    const foto = document.getElementById("fotoInput")
+    foto.value = contato.foto
+    const email = document.getElementById("emailInput")
+    email.value = contato.email
+    const endereco = document.getElementById("enderecoInput")
+    endereco.value = contato.endereco
+    const cidade = document.getElementById("cidadeInput")
+    cidade.value = contato.cidade
+
+    document.getElementById("salvar").addEventListener("click", async () => {
+        try {
+
+            
+        
+            await criarCards()
+
+        } catch (error) {
+            alert(error.message)
+        }
+    })
 }
+
+document.getElementById("salvar").addEventListener("click", async () => {
+    try {
+        const nome = document.getElementById("nomeInput")
+        const telefone = document.getElementById("numeroInput")
+        const foto = document.getElementById("fotoInput")
+        const email = document.getElementById("emailInput")
+        const endereco = document.getElementById("enderecoInput")
+        const cidade = document.getElementById("cidadeInput")
+
+        let validar = validarCampos(nome, telefone, email, endereco, cidade)
+
+        if (!validar.status) {
+            alert(validar.message)
+            return
+        }
+
+        let contato = {
+            nome: nome.value,
+            celular: telefone.value,
+            foto: foto.value,
+            email: email.value,
+            endereco: endereco.value,
+            cidade: cidade.value
+        }
+
+        await postContato(contato)
+
+        nome.value = ""
+        telefone.value = ""
+        foto.value = ""
+        email.value = ""
+        endereco.value = ""
+        cidade.value = ""
+
+        await criarCards()
+
+    } catch (error) {
+        alert(error.message)
+    }
+})
 
 criarCards()
-
